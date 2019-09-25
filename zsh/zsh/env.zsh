6342fbf1daf733619f5063bd3d773a28f0ac532e
maverick <maverick>
2019-09-25 09:43:56 +0900

    make vim look good

--- zsh/zsh/env.zsh
+++ zsh/zsh/env.zsh
@@ -6,8 +6,7 @@ export LC_CTYPE=en_US.UTF-8
 export NPM_PACKAGES="/usr/local/npm_packages"
 export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 export PATH="$NPM_PACKAGES/bin:$PATH"
-
 export HOME=/home/$USER
-
+export TERM=xterm-256color
 export ZSH_CACHE_DIR=$HOME/.zplug/cache
 export GOPATH=$HOME/.config/go
